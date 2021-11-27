5684236ebe411d7799b1f417ea074a7002bf5f55
briannzw <brianwijaya2003@gmail.com>
2021-11-28 02:10:11 +0700

    Add First Version

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
   require 'vendor/autoload.php';
-  require "html_tag_helpers.php";
 ?>
 <html>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery.redirect@1.1.4/jquery.redirect.min.js"></script>
     <head>
-        <title>Rumahin.com</title>
-        <link rel="icon" href="index_img/logo.png">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>Sejarah Indonesia</title>
+        <link rel="icon" href="index_img/icon.png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css-1.css">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="css1.css">
         
         <style>
           .bg-light{
@@ -26,12 +27,11 @@
         </style>
     </head>
     
-    <!-- awal navbar-->
   <main>
-      <nav class="navbar navbar-expand-lg bg-light fixed-top ">
+        <!-- awal navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
           <div class="container-fluid">
-            <img src="index_img/logo.png" height="50px" class="me-2">
-              <h2 class="amiri">Rumahin.com</h2>
+            <h2 class="amiri ms-5 my-2 fs-2">Sejarah</h2>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -40,67 +40,59 @@
                 <li class="nav-item">
                   <a class="nav-link ms-5" aria-current="page" href="#">Halaman Utama</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link ms-5" href="jual.php">Jual</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link ms-5" href="about.php">Tentang Kami</a>
-                </li>
               </ul>
             </div>
           </div>
-      </nav>
+        </nav>
         <!-- akhir navbar-->
     
     
         <!-- awal carousel -->
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="index_img/carousel(1).jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-              <div class="ccaption">
-            <h1 style="font-size: 85px">Rumahin.com</h1>
-            <h5>"Ga punya rumah? Rumahin aja!"</h5>
-            <button id="cbutton" style="font-size: 18px" onclick="location.href = 'jual.php'">Daftar Pencarian</button>
-              </div>
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
           </div>
-        </div>
-        <div class="carousel-item">
-          <img src="index_img/carousel(2).jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-              <div class="ccaption">
-            <h1 style="font-size: 85px">Rumahin.com</h1>
-            <h5>"Ga punya rumah? Rumahin aja!"</h5>
-              <button id="cbutton" style="font-size: 18px" onclick="location.href = 'jual.php'">Daftar Pencarian</button>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://i.pinimg.com/originals/cb/c9/62/cbc96234dcdeb601473001a1c8fc11fd.jpg" width="100%" height="100%" style="object-fit:cover; object-position: 50% 40%;"/>
+              <div class="container">
+                <div class="carousel-caption text-start">
+                  <h2 style="color:black;">"Untuk mencapai sesuatu, harus diperjuangkan dulu. Seperti mengambil buah kelapa, dan tidak menunggu saja seperti jatuh durian yang telah masak.”</h2>
+                  <p class="my-5 amiri" style="color:black;">- Mohammad Natsir<p>
+                </div>
               </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="index_img/carousel(3).jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-              <div class="ccaption">
-            <h1 style="font-size: 85px">Rumahin.com</h1>
-            <h5>"Ga punya rumah? Rumahin aja!"</h5>
-              <button id="cbutton" style="font-size: 18px" onclick="location.href = 'jual.php'">Daftar Pencarian</button>
+            </div>
+            <div class="carousel-item">
+            <img src="https://mmc.tirto.id/image/2018/03/29/ilustrasi-pocut-baren.jpg" width="100%" height="100%" style="object-fit:cover; "/>
+              <div class="container">
+                <div class="carousel-caption">
+                  <h2>"Dalam menghadapi musuh, tak ada yang lebih mengena daripada senjata kasih sayang."</h2>
+                  <p class="my-5 amiri">- Cut Nyak Dien<p>
+                </div>
               </div>
+            </div>
+            <div class="carousel-item">
+            <img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best/v1608981485/hq4agky94t7q1q6yuapg.jpg" width="100%" height="100%" style="object-fit:cover; object-position: 50% 40%;"/>
+
+              <div class="container">
+                <div class="carousel-caption text-end">
+                  <h2 style="color:black;">"Hidup yang tidak dipertaruhkan tidak akan pernah dimenangkan."</h2>
+                  <p class="my-5 amiri" style="color:black;">- Sutan Syahrir</p>
+                </div>
+              </div>
+            </div>
           </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
     <!--akhir carousel-->
 
     <?php
@@ -127,31 +119,41 @@
     <!-- Card Container-->
     <?php 
       \EasyRdf\RdfNamespace::setDefault('og');
+      $i = 0;
     ?>
-    <div class="container-fluid px-5 pb-5" id="containerbg">
-        <h1 class="text-light mb-0 mt-0 py-5">Sejarah Indonesia</h1>
+    
+      <div class="container mt-5">
         <?php foreach($doc->all('dbo:event') as $event) : ?>
           <!--Card -->
           <?php 
-            $wiki_uri = 'https://en.wikipedia.org/wiki/' . str_replace(\EasyRdf\RdfNamespace::get('dbr'), '' ,$event->get('foaf:page'));
+            $i++;
+            $uri = str_replace(\EasyRdf\RdfNamespace::get('dbr'), '' ,$event->get('foaf:page'));
+            $wiki_uri = 'https://en.wikipedia.org/wiki/' . $uri;
             $img_file = \EasyRdf\Graph::newAndLoad($wiki_uri);
           ?>
-          <div class="card my-2">
-            <div class="row cardfont">
-              <div class="col-md-4">
-                <img src="<?= $img_file->image ?>" class="card-img" alt="" onerror="this.onerror=null; this.src='index_img/alt_image.jpg'">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h2><?= $event->get('rdfs:label') ?></h2>
-                  <p class="mb-0"><?= $event->get('dbo:date') ?></p>
-                  <p class="mb-0"><?= $event->get('dbo:place') ?></p>
-                  <a class="bottom-right" href="">Info Selengkapnya</a>
-                </div>
-              </div>
+          <div class="row style=" style="position:relative;">
+            <div class="col-md-7 order-md-2">
+              <h2 class="featurette-heading"><?= $event->get('rdfs:label') ?></h2><br>
+              <p class="lead"><?= $event->get('dbo:date') ?></p>
+              <p class="lead"><?= $event->get('dbo:place') ?></p>
+              <a class="bottom-right link" id="link-<?= $i ?>" data_uri="<?= $uri ?>">Info Selengkapnya</a>
+            </div>
+            <div class="col-md-3 order-md-1">
+              <img src="<?= $img_file->image ?>" class="img-fluid mx-auto cover-img" alt="" onerror="this.onerror=null; this.src='index_img/alt_image.jpg'">
             </div>
           </div>
           <!--End Card-->
+
+          <hr class="my-5">
+          
+          <script>
+            $(function(){
+              $('#link-<?= $i ?>').on("click", function(e){
+                e.preventDefault();
+                $.redirect('info.php', { 'URI' : $('#link-<?= $i ?>').attr("data_uri") });
+              });
+            });
+          </script>
         <?php endforeach; ?>
     </div>
     
@@ -197,35 +199,11 @@
     
     
     <!--awal footer-->
-    <footer class="page-footer bg-dark">
-        <div id="footer">
-          <div class="row">
-        <h1 class="card-title text-white col" id="desFooter">"Ga punya rumah? Rumahin aja!"</h1>
-         <div class="row justify-content-center" style="text-align: center;color: white ">
-                    <div class="col-2" style="font-size: 16px"><b>Contact Us</b></div>
-                    <div class="col-2" style="font-size: 14px">
-                        <a href="#" class="linkFooter"><img src="index_img/fb.png" style="width: 25px;margin-right: 10px;">Rumahin.com</a>
-                    </div>
-                    <div class="col-2" style="font-size: 14px">
-                        <a href="#" class="linkFooter"><img src="index_img/ig.png" style="width: 25px;margin-right: 10px;">Rumahin.com</a>
-                    </div>
-                    <div class="col-2" style="font-size: 14px">
-                        <a href="#" class="linkFooter"><img src="index_img/twitter.png" style="width: 25px;margin-right: 10px;">Rumahin.com</a>
-                    </div>
-                </div>
-                <div class="row justify-content-center" style="margin-top: 2%; text-align: center">
-                    <div class="col-3"><a href="#" class="linkFooter" style="font-size: 15px"><b>Kebijakan dan Privasi</b></a></div>
-                    <div class="col-3"><a href="#" class="linkFooter" style="font-size: 15px"><b>Syarat dan Ketentuan</b></a></div>
-                </div>
-            </div>
-        </div>
-      <div class="card-footer text-muted text-center">
-        <h6><small>Informasi Rumahin disediakan secara eksklusif untuk penggunaan pribadi dan non-komersial konsumen dan tidak boleh digunakan untuk tujuan apa pun selain untuk mengidentifikasi properti prospektif yang mungkin ingin dibeli oleh konsumen. Informasi yang dianggap dapat diandalkan tetapi tidak dijamin akurat. Pembeli untuk memverifikasi semua informasi. Informasi yang diberikan adalah untuk penggunaan pribadi, non-komersial konsumen dan tidak boleh digunakan untuk tujuan apa pun selain untuk mengidentifikasi properti prospektif yang mungkin tertarik untuk dibeli oleh konsumen. Informasi daftar diperbarui setiap 15 menit. Ketentuan Penggunaan & Kebijakan Privasi. Hak Cipta © 2005 - 2021 Rumahin, Inc. Semua hak dilindungi undang-undang</small></h6>
-      </div>
-    </footer>
+        <footer class="page-footer bg-dark">
+            <div id="footer">&copy; 2017–2021 Sejarah, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></div>
+        </footer>
     <!--akhir footer-->
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
       $(window).scroll(function(){
         $('nav').toggleClass('scroll', $(this).scrollTop() > 700);
---
+++ info.php
@@ -0,0 +1,237 @@
+<?php 
+    require 'vendor/autoload.php';
+    require "html_tag_helpers.php";
+
+    $uri="";
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if($_POST['URI']){
+            $uri = $_POST['URI'];
+        //    echo "<script>alert('".$uri."');</script>";
+        }
+    }
+    else{
+        var_dump('POST Data not found!');
+        header('Location: index.php');
+        die;
+    }
+    \EasyRdf\RdfNamespace::set("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+    \EasyRdf\RdfNamespace::set("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+    \EasyRdf\RdfNamespace::set("dbo", "http://dbpedia.org/ontology/");
+    \EasyRdf\RdfNamespace::set("dbr", "http://dbpedia.org/resource/");
+    \EasyRdf\RdfNamespace::set("foaf", "http://xmlns.com/foaf/0.1/");
+    \EasyRdf\RdfNamespace::set("geo", "http://www.w3.org/2003/01/geo/wgs84_pos#");
+    
+    \EasyRdf\RdfNamespace::setDefault('og');
+
+    $wiki_uri = 'https://en.wikipedia.org/wiki/' . $uri;
+    $img_file = \EasyRdf\Graph::newAndLoad($wiki_uri);
+    
+    $sparql = new \EasyRdf\Sparql\Client('https://dbpedia.org/sparql');
+
+    $query = "
+    SELECT DISTINCT *
+    WHERE {
+        <".\EasyRdf\RdfNamespace::get("dbr").$uri."> rdfs:label ?label;
+                     dbo:abstract ?abstract;
+                     dbo:place ?tmp_place.
+        OPTIONAL{<".\EasyRdf\RdfNamespace::get("dbr").$uri."> dbo:date ?date.}
+        OPTIONAL{<".\EasyRdf\RdfNamespace::get("dbr").$uri."> dbp:casualties ?casuality.}
+        OPTIONAL{<".\EasyRdf\RdfNamespace::get("dbr").$uri."> dbo:result ?result.}
+        ?tmp_place   rdfs:label ?place;
+                     geo:lat ?lat;
+                     geo:long ?long.
+        FILTER NOT EXISTS {?tmp_place dbo:currencyCode 'IDR'}
+        FILTER(LANG(?label) = 'in' || LANG(?label) = 'en' || LANG(?label) = '').
+        FILTER(LANG(?abstract) = 'in' || LANG(?abstract) = 'en' || LANG(?abstract) = '')
+    }
+    ORDER BY DESC(?casuality)
+    ";
+    
+    $result = $sparql->query($query);
+
+    $detail = [];
+    foreach($result as $row){
+        $detail = [
+            'label'=> isset($row->label) ? $row->label : "Tidak Diketahui",
+            'abstract'=> isset($row->abstract) ? $row->abstract : "Tidak Diketahui",
+            'place'=> isset($row->place) ? $row->place : "Tidak Diketahui",
+            'date'=> isset($row->date) ? $row->date : "Tidak Diketahui",
+            'casuality'=> isset($row->casuality) ? $row->casuality : "Tidak Diketahui",
+            'result'=> isset($row->result) ? $row->result : "Tidak Diketahui",
+            'lat'=> isset($row->lat) ? $row->lat : -6.166667,
+            'long'=> isset($row->long) ? $row->long : 106.816666,
+        ];
+        break; //ambil hanya 1 data
+    }
+?>
+
+<html>
+    <head>
+        <title><?= $detail['label'] ?></title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet"> 
+        <!--Link ke Bootstrap-->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!--Akhir link ke Bootstrap-->
+        
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin=""/>
+
+         <!-- Make sure you put this AFTER Leaflet's CSS -->
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossorigin=""></script>
+
+        <!--CSS-->
+            <style>
+                .amiri{
+                    font-family: 'Amiri', serif;
+                    margin: auto;
+                    font-size: 20px;
+                }
+                .containerText,p,#footer{
+                    font-family: 'Raleway', sans-serif;
+                }
+                #judul{
+                    font-size: 40px;
+                }
+                table,tr,td{
+                    padding: 10px;
+                    font-size: 23px;
+                }
+                .bg-light{
+                    background: transparent !important;
+                    transition:0.5s ease;
+                }
+                .bg-light.scroll{
+                    background: white !important;
+                }
+                p{
+                    text-align: right;
+                }
+                #footer{
+                    color:white;
+                    text-align: center;
+                    padding: 20px;
+                }
+                a:active, a:link, a:visited, a:hover{
+                    color: white;
+                    text-decoration: none;
+                    
+                }
+                #map { height: 180px; }
+            </style>
+        <!--Akhir CSS-->
+    </head>
+    <body>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+            <div class="container-fluid">
+                <h2 class="amiri ms-5 my-2 fs-2">Sejarah</h2>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link ms-5" aria-current="page" href="index.php">Halaman Utama</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(window).scroll(function(){
+        $('nav').toggleClass('scroll', $(this).scrollTop() > 200);
+        })
+    </script>
+        <!--Akhir Navbar-->
+        <!--Container Gambar-->
+            <div class="container-fluid" style="margin-bottom: 5%;">
+                <div class="row">
+                    <div class="col" style="background-color:gainsboro">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-9 mx-auto">
+                                    <img src="<?= $img_file->image ?>" style="width: 100%;" onerror="this.onerror=null; this.src='index_img/alt_image.jpg'">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!--Akhir Container Gambar-->
+        <!--Awal Container Teks-->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-9 mx-auto">
+                                    <h1 class="containerText" id="judul"><b><?= $detail['label'] ?></b></h1><br><br>
+                                    <table>
+                                        <tr>
+                                            <td class="containerText">Tempat</td>
+                                            <td class="containerText">:</td>
+                                            <td class="containerText"><?= $detail['place'] ?></td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <td class="containerText">Tanggal</td>
+                                            <td class="containerText">:</td>
+                                            <td class="containerText"><?= $detail['date'] ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="containerText">Kasualitas</td>
+                                            <td class="containerText">:</td>
+                                            <td class="containerText"><?= $detail['casuality'] ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="containerText">Hasil</td>
+                                            <td class="containerText">:</td>
+                                            <td class="containerText"><?= $detail['result'] ?></td>
+                                        </tr>
+                                    </table>
+                                    <br>
+                                    <p><?= $detail['abstract'] ?></p>
+                                    
+                                    <!-- Map -->
+                                    <?php
+                                    print "<div id='map'></div>";
+                                    $map_script = "var mymap = L.map('map').setView([" . $detail['lat'] . ", " . $detail['long'] . "], 13);
+                                            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                                            maxZoom: 18,
+                                            attribution: 'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, ' +
+                                                    '<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, ' +
+                                                        'Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
+                                            id: 'mapbox/streets-v11',
+                                            tileSize: 512,
+                                            zoomOffset: -1
+                                        }).addTo(mymap);
+
+                                        L.marker([" . $detail['lat'] . ", " . $detail['long'] . "]).addTo(mymap)
+                                        .bindPopup(\"<b>" . $detail['place'] . "</b>\").openPopup();";
+
+                                    print "<script>" . $map_script . "</script>";
+
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!--Akhir Container Teks-->
+        <br>
+        <!--Footer-->
+        <footer class="page-footer bg-dark">
+            <div id="footer">&copy; 2017–2021 Sejarah, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></div>
+        </footer>
+        <!--Akhir Footer-->
+
+    </body>
+</html>
